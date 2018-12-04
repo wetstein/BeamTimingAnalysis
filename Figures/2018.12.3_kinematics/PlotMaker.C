e79352bc5af92c3f0b856276a6b249d5764f68f7
wetstein <wetstein@wetsteins-MacBook-Air.local>
2018-12-03 19:01:33 -0600

    modified the deltaTvsE scripts

--- Figures/2018.12.3_kinematics/PlotMaker.C
+++ Figures/2018.12.3_kinematics/PlotMaker.C
@@ -1,5 +1,5 @@
 {
-  TF1* KinemFunc = new TF1("KinemFunc","((x/139.4)-sqrt((x^2/139.4^2)-1))*26.033",139.,10000.);
+  TF1* KinemFunc = new TF1("KinemFunc","((x/0.1394)-sqrt((x^2/0.1394^2)-1))*26.033",139.,10000.);
   KinemFunc->Draw();
 
 
